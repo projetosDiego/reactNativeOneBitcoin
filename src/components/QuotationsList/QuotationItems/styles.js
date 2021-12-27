fcb308d9f84ead64a2aa3a31c6ec4e3582def293
Diegom <diego_r.g@hotmail.com>
2021-12-27 15:22:45 +0100

    Inital commit

---
+++ src/components/QuotationsList/QuotationItems/styles.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    mainContent: {
+        width: "95%",
+        height: "auto",
+        backgroundColor: "#000000",
+        marginLeft: "3%",
+        marginBottom: 15,
+        borderRadius: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 10
+    },
+    contextLeft: {
+        width: "36%",
+        alignItems: "flex-start"
+    },
+    boxLogo: {
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    logoBitcoin: {
+        width: 40,
+        height: 40,
+        marginLeft: 2
+    },
+    dayCotation:{
+        fontSize: 16,
+        paddingLeft: 10,
+        color: "#ffffff",
+        fontWeight: "bold"
+    },
+    contextRight: {
+        width: "60%",
+        alignItems: "flex-end"
+    },
+    price: {
+        color: "#ffffff",
+        fontSize: 18,
+        fontWeight: "bold"
+    }
+})
+
+export default styles
---
+++ src/components/QuotationsList/index.js
@@ -0,0 +1,56 @@
+import React, { Fragment } from "react"
+import { View, Text, ScrollView, TouchableOpacity, FlatList } from "react-native"
+import QuotationItems from "./QuotationItems"
+import styles from "./styles"
+
+export default function QuatationList(props){
+    const daysQuery = props.filterDay
+    return(
+        <Fragment>
+            <View style={styles.filters}>
+                <TouchableOpacity
+                    style={styles.buttonQuery}
+                    onPress={() => daysQuery(7)}
+                >
+                    <Text style={styles.textButtonQuery}>7D</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.buttonQuery}
+                    onPress={() => daysQuery(15)}
+                >
+                    <Text style={styles.textButtonQuery}>15D</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.buttonQuery}
+                    onPress={() => daysQuery(30)}
+                >
+                    <Text style={styles.textButtonQuery}>1M</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.buttonQuery}
+                    onPress={() => daysQuery(90)}
+                >
+                    <Text style={styles.textButtonQuery}>3M</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.buttonQuery}
+                    onPress={() => daysQuery(180)}
+                >
+                    <Text style={styles.textButtonQuery}>6M</Text>
+                </TouchableOpacity>
+            </View>
+            <ScrollView>
+                <FlatList 
+                    data={props.listTransactions}
+                    renderItem={({item}) => {
+                        return <QuotationItems valor={item.valor} data={item.date}/>
+                    }}
+                />
+            </ScrollView>
+        </Fragment>
+    )
+}
